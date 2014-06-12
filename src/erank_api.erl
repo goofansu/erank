709d0bc9880765920519a306017714d27fd67666
goofansu <goofan.su@gmail.com>
2014-06-12 16:24:52 +0800

    Member -> Identity

--- src/erank_api.erl
+++ src/erank_api.erl
@@ -21,34 +21,34 @@
 %%%===================================================================
 
 %% 增加指定排行榜的指定玩家的分数
-incr_score(RankType, Member, Increment) ->
-    Score = new_score(RankType, Member, Increment),
-    eredis_api:zadd(RankType, Score, Member).
+incr_score(RankType, Identity, Increment) ->
+    Score = new_score(RankType, Identity, Increment),
+    eredis_api:zadd(RankType, Score, Identity).
 
 %% 获得指定排行榜的指定玩家的分数
-get_score(RankType, Member) ->
-    case eredis_api:zscore(RankType, Member) of
+get_score(RankType, Identity) ->
+    case eredis_api:zscore(RankType, Identity) of
         {ok, undefined} -> 0;
         {ok, Val} -> erank_misc:realworld_score(Val)
     end.
 
 %% 获得指定排行榜的指定玩家的排名
 -spec get_rank(atom(), term()) -> integer() | not_found.
-get_rank(RankType, Member) ->
-    case eredis_api:zrevrank(RankType, Member) of
+get_rank(RankType, Identity) ->
+    case eredis_api:zrevrank(RankType, Identity) of
         {ok, undefined} -> 0;
         {ok, Val} -> erank_misc:realworld_rank(Val)
     end.
 
 %% 获得指定排行榜的指定玩家的排名和分数
-get_rank_score(RankType, Member) ->
-    [{ok,Rank}, {ok,Score}] = eredis_api:pipeline_rank_score(RankType, Member),
+get_rank_score(RankType, Identity) ->
+    [{ok,Rank}, {ok,Score}] = eredis_api:pipeline_rank_score(RankType, Identity),
     [erank_misc:realworld_rank(Rank),
      erank_misc:realworld_score(Score)].
 
 %% 获得前一个玩家的信息
-get_previous_member(RankType, Member) ->
-    case get_rank(RankType, Member) of
+get_previous_member(RankType, Identity) ->
+    case get_rank(RankType, Identity) of
         0 -> {error, rank_not_found};
         1 -> {error, rank_first};
         Rank ->
@@ -68,18 +68,20 @@ get_score_by_rank(RankType, Rank) ->
 %% 获得指定分数段内的玩家信息列表
 list_member_range_by_score(RankType, Max, Min) ->
     {ok, L} = eredis_api:zrevrangebyscore(RankType, Max, Min),
-    lists:map(fun(Member)-> binary_to_term(Member) end, L).
+    lists:map(fun(Identity)-> binary_to_term(Identity) end, L).
 
 %% 获得指定分数段内的玩家信息列表，限制返回个数
 list_member_range_by_score(RankType, Max, Min, Limit) ->
     {ok, L} = eredis_api:zrevrangebyscore(RankType, Max, Min, Limit),
-    lists:map(fun(Member)-> binary_to_term(Member) end, L).
+    L1 = lists:map(fun(Identity)-> binary_to_term(Identity) end, L),
+    {ok, Nicknames} = eredis_api:mget_nicknames(L1),
+    lists:zip(L1, Nicknames).
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
-new_score(RankType, Member, Increment) ->
-    {ok, Val} = eredis_api:zscore(RankType, Member),
+new_score(RankType, Identity, Increment) ->
+    {ok, Val} = eredis_api:zscore(RankType, Identity),
     Score = erank_misc:realworld_score(Val),
     erank_misc:redis_score(Score+Increment).
