3444d99108ddf061728ef53dbada3c3bb075b7ca
goofansu <goofan.su@gmail.com>
2014-06-20 16:55:59 +0800

    修复数据

--- src/erank_util.erl
+++ src/erank_util.erl
@@ -19,12 +19,19 @@ clear_rank(RankType) ->
     Key = eredis_keygen:rank_key(RankType),
     eredis_api:del(Key).
 
-fix_consume_rank([]) -> ok;
-fix_consume_rank([{Identity, InvalidScore}|T]) ->
+fix_consume_rank(L) ->
+    RankKey = eredis_keygen:rank_key(consume),
+    {ok, C} = eredis:start_link(),
+    eredis:q(C, ["DEL", RankKey]),
+    fix_consume_rank_1(L, C, RankKey).
+
+fix_consume_rank_1([], _C, _RankKey) -> ok;
+fix_consume_rank_1([{Identity, Score}|T], C, RankKey) ->
+    RedisScore = erank_misc:redis_score(Score),
     {AccName, SN} = Identity,
     IdentityTerm = {binary_to_list(AccName), SN},
-    erank_api:incr_score(consume, IdentityTerm, -InvalidScore),
-    fix_consume_rank(T).
+    {ok, <<"1">>} = eredis:q(C, ["ZADD", RankKey, RedisScore, IdentityTerm]),
+    fix_consume_rank_1(T, C, RankKey).
 
 %%%===================================================================
 %%% Internal functions
